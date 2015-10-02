4f9f7508ae3359df9e8cefd54d987d46b3330948
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2015-10-02 23:37:50 +0900

    wip

--- Chapter22Reconstruction/src/Main.hs
+++ Chapter22Reconstruction/src/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
-data Term = Var String
+import Data.Set (Set, (\\))
+import qualified Data.Set as S
+
+type VarName = String
+
+data Term = Var VarName
           | Zero
           | TTrue
           | TFalse
@@ -8,10 +13,33 @@ data Term = Var String
           | Pred Term
           | If Term Term Term
           | Lambda String Term
-          | Let String Term Term
+          | Let VarName Term Term
 
-data Type = TypeVar String
-          | Nam
+data Type = TypeVar VarName
+          | Nat
           | TBool
           | Arrow Type Type
-          | Scheme [String] Type
+          | Scheme (Set VarName) Type
+
+type Context = Set (VarName, Type)
+
+type Constraint = (Type, Type)
+
+type Assign = (VarName, Type)
+
+ftv :: Type -> Set VarName
+ftv (TypeVar v) = S.singleton v
+ftv Nat = S.empty
+ftv TBool = S.empty
+ftv (Arrow t1 t2) = S.union (ftv t1) (ftv t2)
+ftv (Scheme vs t) = ftp t \\ vs
+
+unify :: Set Constraint -> Maybe [Assign]
+unify c =
+  if S.null c
+  then []
+  else
+    let (s, t)
+
+ct :: Context -> Term -> (Type, Set VarName, Set Constraint)
+ct = undefined
