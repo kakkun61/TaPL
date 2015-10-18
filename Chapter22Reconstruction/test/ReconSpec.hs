b59a96b69829cad793a85f2f7ff117cb0844d4bb
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2015-10-18 16:59:59 +0900

    pick up vars

--- Chapter22Reconstruction/test/ReconSpec.hs
+++ Chapter22Reconstruction/test/ReconSpec.hs
@@ -14,78 +14,116 @@ spec = do
     describe "success" $ do
       it "x : Nat ⊦ x : Nat | ∅ {}" $ do
         let
-          c = Context $ M.singleton (ValueVarName 0) Nat
-          t = Var $ ValueVarName 0
-        runState (runExceptT $ ctype c t) (TypeVarNameSeed $ TypeVarName 0) `shouldBe` (Right (Nat, S.empty), TypeVarNameSeed $ TypeVarName 0)
+          ctx   = Context $ M.singleton (ValueVarName 0) Nat
+          term  = Var $ ValueVarName 0
+          seed  = TypeVarNameSeed $ TypeVarName 0
+          typ   = Nat
+          cons  = S.empty
+          seed' = TypeVarNameSeed $ TypeVarName 0
+        runState (runExceptT $ ctype ctx term) seed `shouldBe` (Right (typ, cons), seed')
 
       it "x : Bool ⊦ x : Bool | ∅ {}" $ do
         let
-          c = Context $ M.singleton (ValueVarName 0) TBool
-          t = Var $ ValueVarName 0
-        runState (runExceptT $ ctype c t) (TypeVarNameSeed $ TypeVarName 0) `shouldBe` (Right (TBool, S.empty), TypeVarNameSeed $ TypeVarName 0)
+          ctx   = Context $ M.singleton (ValueVarName 0) TBool
+          term = Var $ ValueVarName 0
+          seed  = TypeVarNameSeed $ TypeVarName 0
+          typ   = TBool
+          cons  = S.empty
+          seed' = TypeVarNameSeed $ TypeVarName 0
+        runState (runExceptT $ ctype ctx term) seed `shouldBe` (Right (typ, cons), seed')
 
       it "x : X ⊦ x : X | ∅ {}" $ do
         let
-          c = Context $ M.singleton (ValueVarName 0) (TypeVar $ TypeVarName 0)
-          t = Var $ ValueVarName 0
-        runState (runExceptT $ ctype c t) (TypeVarNameSeed $ TypeVarName 1) `shouldBe` (Right (TypeVar $ TypeVarName 0, S.empty), TypeVarNameSeed $ TypeVarName 1)
+          ctx = Context $ M.singleton (ValueVarName 0) (TypeVar $ TypeVarName 0)
+          term = Var $ ValueVarName 0
+          seed  = TypeVarNameSeed $ TypeVarName 1
+          typ   = TypeVar $ TypeVarName 0
+          cons  = S.empty
+          seed' = TypeVarNameSeed $ TypeVarName 1
+        runState (runExceptT $ ctype ctx term) seed `shouldBe` (Right (typ, cons), seed')
 
       it "x : ∀X. X ⊦ x : ∀S.S | {S} {}" $ do
         let
-          c = Context $ M.singleton (ValueVarName 0) (Scheme (S.singleton $ TypeVarName 0) (TypeVar $ TypeVarName 0))
-          t = Var $ ValueVarName 0
-        runState (runExceptT $ ctype c t) (TypeVarNameSeed $ TypeVarName 0) `shouldBe` (Right (TypeVar $ TypeVarName 0, S.empty), TypeVarNameSeed $ TypeVarName 1)
+          ctx   = Context $ M.singleton (ValueVarName 0) (Scheme (S.singleton $ TypeVarName 0) (TypeVar $ TypeVarName 0))
+          term  = Var $ ValueVarName 0
+          seed  = TypeVarNameSeed $ TypeVarName 0
+          typ   = TypeVar $ TypeVarName 0
+          cons  = S.empty
+          seed' = TypeVarNameSeed $ TypeVarName 1
+        runState (runExceptT $ ctype ctx term) seed `shouldBe` (Right (typ, cons), seed')
 
       it "x : ∀X Y. X ⊦ x : S1 | {S1, S2} {}" $ do
         let
-          c = Context $ M.singleton (ValueVarName 0) (Scheme (S.fromList $ map TypeVarName [0, 1]) (TypeVar $ TypeVarName 0))
-          t = Var $ ValueVarName 0
-        runState (runExceptT $ ctype c t) (TypeVarNameSeed $ TypeVarName 0) `shouldBe` (Right (TypeVar $ TypeVarName 0, S.empty), TypeVarNameSeed $ TypeVarName 2)
+          ctx   = Context $ M.singleton (ValueVarName 0) (Scheme (S.fromList $ map TypeVarName [0, 1]) (TypeVar $ TypeVarName 0))
+          term  = Var $ ValueVarName 0
+          seed  = TypeVarNameSeed $ TypeVarName 0
+          typ   = TypeVar $ TypeVarName 0
+          cons  = S.empty
+          seed' = TypeVarNameSeed $ TypeVarName 2
+        runState (runExceptT $ ctype ctx term) seed `shouldBe` (Right (typ, cons), seed')
 
       it "x : ∀X Y. X → Y ⊦ x : S1 → S2 | {S1, S2} {}" $ do
         let
-          c = Context $ M.singleton (ValueVarName 0) (Scheme (S.fromList $ map TypeVarName [0, 1]) (Arrow (TypeVar $ TypeVarName 0) (TypeVar $ TypeVarName 1)))
-          t = Var $ ValueVarName 0
-        runState (runExceptT $ ctype c t) (TypeVarNameSeed $ TypeVarName 0) `shouldBe` (Right (Arrow (TypeVar $ TypeVarName 0) (TypeVar $ TypeVarName 1), S.empty), TypeVarNameSeed $ TypeVarName 2)
+          ctx   = Context $ M.singleton (ValueVarName 0) (Scheme (S.fromList $ map TypeVarName [0, 1]) (Arrow (TypeVar $ TypeVarName 0) (TypeVar $ TypeVarName 1)))
+          term  = Var $ ValueVarName 0
+          seed  = TypeVarNameSeed $ TypeVarName 0
+          typ   = Arrow (TypeVar $ TypeVarName 0) (TypeVar $ TypeVarName 1)
+          cons  = S.empty
+          seed' = TypeVarNameSeed $ TypeVarName 2
+        runState (runExceptT $ ctype ctx term) seed `shouldBe` (Right (typ, cons), seed')
 
     describe "failure" $ do
       it "∅ ⊦ x ⇒ fail" $ do
         let
-          c = Context $ M.empty
-          t = Var $ ValueVarName 0
-        evalState (runExceptT $ ctype c t) (TypeVarNameSeed $ TypeVarName 0) `shouldSatisfy` isLeft
+          ctx  = Context $ M.empty
+          term = Var $ ValueVarName 0
+          seed = TypeVarNameSeed $ TypeVarName 0
+        evalState (runExceptT $ ctype ctx term) seed `shouldSatisfy` isLeft
 
   describe "CT-Abs" $ do
     describe "success" $ do
       it "∅ ⊦ λx. x : S → S | {S} {}" $ do
         pending
         let
-          c = Context $ M.empty
-          t = Lambda (ValueVarName 0) (Var $ ValueVarName 0)
-        runState (runExceptT $ ctype c t) (TypeVarNameSeed $ TypeVarName 0) `shouldBe` (Right (Arrow (TypeVar $ TypeVarName 0) (TypeVar $ TypeVarName 1), S.empty), TypeVarNameSeed $ TypeVarName 2)
+          ctx   = Context $ M.empty
+          term  = Lambda (ValueVarName 0) (Var $ ValueVarName 0)
+          seed  = TypeVarNameSeed $ TypeVarName 0
+          typ   = Arrow (TypeVar $ TypeVarName 0) (TypeVar $ TypeVarName 0)
+          cons  = S.empty
+          seed' = TypeVarNameSeed $ TypeVarName 1
+        runState (runExceptT $ ctype ctx term) seed `shouldBe` (Right (typ, cons), seed')
 
     describe "failure" $ do
       it "∅ ⊦ λx. y ⇒ fail" $ do
         pending
         let
-          c = Context $ M.empty
-          t = Lambda (ValueVarName 0) (Var $ ValueVarName 1)
-        evalState (runExceptT $ ctype c t) (TypeVarNameSeed $ TypeVarName 0) `shouldSatisfy` isLeft
+          ctx  = Context $ M.empty
+          term = Lambda (ValueVarName 0) (Var $ ValueVarName 1)
+          seed = TypeVarNameSeed $ TypeVarName 0
+        evalState (runExceptT $ ctype ctx term) seed `shouldSatisfy` isLeft
 
   describe "CT-App" $ do
     describe "success" $ do
       it "∅ ⊦ λx. true 0 : S | {T, S} {T → Bool = Nat → S}" $ do
-         pending
-         let
-           c = Context $ M.empty
-           t = App (Lambda (ValueVarName 0) TTrue) Zero
-         runState (runExceptT $ ctype c t) (TypeVarNameSeed $ TypeVarName 0) `shouldBe` (Right ((TypeVar $ TypeVarName 1), S.singleton $ Constraint (Arrow (TypeVar $ TypeVarName 0) TBool) (Arrow Nat (TypeVar $ TypeVarName 1))), TypeVarNameSeed $ TypeVarName 2)
+        pending
+        let
+          ctx  = Context $ M.empty
+          term  = App (Lambda (ValueVarName 0) TTrue) Zero
+          seed  = TypeVarNameSeed $ TypeVarName 0
+          typ   = TypeVar $ TypeVarName 1
+          cons  = S.singleton $ Constraint (Arrow (TypeVar $ TypeVarName 0) TBool) (Arrow Nat (TypeVar $ TypeVarName 1))
+          seed' = TypeVarNameSeed $ TypeVarName 2
+        runState (runExceptT $ ctype ctx term) seed `shouldBe` (Right (typ, cons), seed')
 
   describe "CT-Zero" $ do
     describe "success" $ do
       it "∅ ⊦ 0 : Nat | ∅ {}" $ do
         pending
         let
-          c = Context $ M.empty
-          t = Zero
-        runState (runExceptT $ ctype c t) (TypeVarNameSeed $ TypeVarName 0) `shouldBe` (Right (Nat, S.empty), TypeVarNameSeed $ TypeVarName 0)
+          ctx   = Context $ M.empty
+          term  = Zero
+          seed  = TypeVarNameSeed $ TypeVarName 0
+          typ   = Nat
+          cons  = S.empty
+          seed' = TypeVarNameSeed $ TypeVarName 0
+        runState (runExceptT $ ctype ctx term) seed `shouldBe` (Right (typ, cons), seed')
