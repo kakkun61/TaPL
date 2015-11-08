157f84c77e4cc95ec7ef6ed6ecb626cf3faf390c
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2015-11-09 04:49:21 +0900

    add integration test (1 failure)

---
+++ Chapter22Reconstruction/test/IntegrationSpec.hs
@@ -0,0 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module IntegrationSpec where
+
+import Test.Hspec
+import Recon
+import Data.Bifunctor
+import qualified Data.Map as M
+
+spec :: Spec
+spec = do
+  describe "success" $ do
+    it "\"let 0 = \\0. 0 in 0 true\" : Bool" $ do
+      let
+        script = "let 0 = \\0. 0 in 0 true"
+      do
+        term <- bimap show id $ parse script
+        prinso (Context M.empty) term
+      `shouldSatisfy` \a ->
+        case a of
+          Right (_, typ) -> typ == TBool
+          Left _ -> False
+
+    it "\"let 0 = \\0. 0 in 0 true[LF]\" : Bool" $ do
+      let
+        script = "let 0 = \\0. 0 in 0 true\n"
+      do
+        term <- bimap show id $ parse script
+        prinso (Context M.empty) term
+      `shouldSatisfy` \a ->
+        case a of
+          Right (_, typ) -> typ == TBool
+          Left _ -> False
